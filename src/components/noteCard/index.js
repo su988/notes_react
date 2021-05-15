14991ba7a8d82b4f836b3d5667c705d85629851e
Suyash S <suyash988@gmail.com>
2021-05-15 18:42:14 -0400

    style card with checkox

--- src/components/noteCard/index.js
+++ src/components/noteCard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@material-ui/core/Card';
 import IconButton from '@material-ui/core/IconButton';
 import CardActions from '@material-ui/core/CardActions';
@@ -6,27 +6,42 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
 
 import useStyles from './styles';
 
 function NoteCard({
-  note: { title, description, category, date },
+  note: { title, description, category, date, completed },
   handleOpen,
 }) {
   const classes = useStyles();
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
   return (
     <>
       <Card className={classes.root}>
-        <CardContent className={classes[category]}>
+        <CardContent className={checked ? classes.done : classes[category]}>
           <div className={classes.header}>
-            <Typography variant="h5" component="h2" className={classes.title}>
-              {title}
-            </Typography>
-            <CardActions className={classes.actionContainer}>
-              <IconButton onClick={handleOpen}>
+            <div className={classes.headerLeft}>
+              <Checkbox
+                checked={checked}
+                onChange={handleChange}
+                className={classes.checkbox}
+              />
+              <Typography variant="h5" component="h2" className={classes.title}>
+                {title}
+              </Typography>
+            </div>
+
+            <CardActions className={classes.headerRight}>
+              <IconButton onClick={handleOpen} className={classes.editBtn}>
                 <EditIcon />
               </IconButton>
-              <IconButton>
+              <IconButton className={classes.deleteBtn}>
                 <DeleteIcon />
               </IconButton>
             </CardActions>
--- src/components/noteCard/styles.js
+++ src/components/noteCard/styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: '404px',
-    minHeight: '174px',
+    maxHeight: '174px',
     boxShadow: '0px 3px 6px #00000029',
     borderRadius: '4px',
     color: '#fff',
@@ -11,6 +11,12 @@ const useStyles = makeStyles((theme) => ({
     '&:nth-child(odd)': {
       marginRight: '15px',
     },
+  },
+
+  done: {
+    backgroundColor: '#282E2999',
+    minHeight: '100%',
+    textDecoration: 'line-through',
   },
 
   work: {
@@ -32,17 +38,41 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
+    marginTop: '-8px',
+  },
+
+  headerLeft: {
+    display: 'flex',
+    alignItems: 'center',
+    marginLeft: '-13px',
+  },
+
+  checkbox: {
+    marginRight: '5px',
+    color: '#fff',
+    opacity: '0.6',
   },
 
   title: {
     fontSize: '20px',
   },
 
-  actionContainer: {
+  headerRight: {
     opacity: '0.6',
+    marginRight: '-10px',
   },
 
-  desctiption: { fontSize: '14px' },
+  editBtn: {
+    padding: 0,
+    color: '#FFFFFF',
+  },
+
+  deleteBtn: {
+    padding: 0,
+    color: '#FFFFFF',
+  },
+
+  desctiption: { fontSize: '14px', minHeight: '90px' },
 
   date: { opacity: '0.6', fontSize: '15px' },
 }));
